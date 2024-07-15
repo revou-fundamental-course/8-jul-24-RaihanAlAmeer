2421dd5b7d6cc4e10645aeea328d119c9bc76c7f
Muhammad Raihan Al Ameer <77021024+RaihanAlAmeer@users.noreply.github.com>
2024-07-15 17:09:07 +0700

    adding js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,11 @@
 // ini javascript
+// ambil value input
+const inputradio1 = document.getElementById("section-man");
+const inputradio2 = document.getElementById("section-woman");
+const berat = document.getElementById("beratbadan")
+const usia = document.getElementById("usia")
+const tinggi = document.getElementById("tinggi")
+
+var bmi;
+
+bmi = berat / tinggi *Math.sqrt(2);
