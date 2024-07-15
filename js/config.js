3d96df1516952837b6954b0949ca79edc81ba836
Muhammad Raihan Al Ameer <77021024+RaihanAlAmeer@users.noreply.github.com>
2024-07-15 21:20:15 +0700

    js half fix

--- js/config.js
+++ js/config.js
@@ -1,11 +1,24 @@
 // ini javascript
 // ambil value input
-const inputradio1 = document.getElementById("section-man");
-const inputradio2 = document.getElementById("section-woman");
-const berat = document.getElementById("beratbadan")
-const usia = document.getElementById("usia")
-const tinggi = document.getElementById("tinggi")
+// const inputradio1 = document.getElementById("section-man");
+// const inputradio2 = document.getElementById("section-woman");
 
-var bmi;
 
-bmi = berat / tinggi *Math.sqrt(2);
+function showalert(){
+//  ambil value input html   
+    const berat = document.getElementById('berat').value;
+    const usia = document.getElementById('usia').value;
+    const tinggi = document.getElementById('tinggi').value;
+
+    // convert meter ke cm
+    const cmtinggi = tinggi / 100;
+    
+    // hitung vmi
+    let bmi = berat / (cmtinggi * cmtinggi); 
+    let fixbmi = bmi.toFixed(1); 
+
+ document.getElementById('perhitungan').innerHTML = fixbmi;
+
+    
+
+}
